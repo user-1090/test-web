91d5133c8432180d264b0776201f5f9f545fa9d6
user-1090 <2420770767@qq.com>
2026-05-18 19:11:49 +0800

    设置模板页面pages

---
+++ scripts/page.js
@@ -0,0 +1,62 @@
+const params = new URLSearchParams(window.location.search);
+const type = params.get("type");
+
+const pages = {
+    eat: {
+        title: "吃饭",
+        list: [
+            {
+                text: "米饭",
+                link: "rice"
+            },
+            {
+                text: "面条",
+                link: "noodle"
+            },
+            {
+                text: "水",
+                link: "water"
+            }
+        ]
+    },
+
+    sleep: {
+        title: "睡觉",
+        list: [
+            {
+                text: "床单",
+                link: "bed"
+            },
+            {
+                text: "被子",
+                link: "quilt"
+            },
+            {
+                text: "枕头",
+                link: "pillow"
+            }
+        ]
+    }
+};
+
+const currentPage = pages[type];
+
+document.getElementById("title").textContent = currentPage.title;
+
+const list = document.getElementById("list");
+
+currentPage.list.forEach(item => {
+
+    const li = document.createElement("li");
+
+    const a = document.createElement("a");
+
+    a.textContent = item.text;
+
+    a.href = `page.html?type=${item.link}`;
+
+    li.appendChild(a);
+
+    list.appendChild(li);
+
+});
